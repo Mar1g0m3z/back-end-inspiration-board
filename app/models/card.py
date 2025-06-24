b08a8f02012519135f24b2083ab22592fb06cd18
Xinshuang Jin <jinxinshuang@gmail.com>
2025-06-23 21:02:41 -0400

    add to_dict and from_dict methods to Card model in app/models/card.py

--- app/models/card.py
+++ app/models/card.py
@@ -9,3 +9,20 @@ class Card(db.Model):
     likes_count: Mapped[int]
     board_id: Mapped[Optional[int]]=mapped_column(ForeignKey("board.id"))
     board: Mapped[Optional["Board"]] = relationship(back_populates="cards")
+
+    def to_dict(self):
+        card_as_dict = {}
+
+        card_as_dict["id"] = self.id
+        card_as_dict["message"] = self.message
+        card_as_dict["likes_count"] = self.likes_count
+        card_as_dict["board_id"] = self.board_id
+
+        return card_as_dict
+
+    @classmethod
+    def from_dict(cls, card_data):
+        
+        return cls(message=card_data["message"],
+                   likes_count=card_data["likes_count"],
+                   board_id=card_data["board_id"])
