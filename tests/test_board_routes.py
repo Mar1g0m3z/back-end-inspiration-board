0613a09b87270c0117eaf0e4c550d156da8f29cb
sashavershkova <a.m.vershkova@gmail.com>
2025-07-02 19:21:40 -0700

    add test_get_all_boards_no_saved_boards, pass it

--- tests/test_board_routes.py
+++ tests/test_board_routes.py
@@ -2,6 +2,11 @@ from app.models.board import Board
 from app.db import db
 import pytest
 
+def test_get_all_boards_no_saved_boards(client):
+    # Act
+    response = client.get("/boards")
+    response_body = response.get_json()
 
-
-
+    # Assert
+    assert response.status_code == 200
+    assert response_body == []
