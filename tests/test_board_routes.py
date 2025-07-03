ad2d8f0aa7a647073c6aed365deb66dd22f17eae
sashavershkova <a.m.vershkova@gmail.com>
2025-07-02 19:23:32 -0700

    add test_get_all_boards_one_saved_board, pass it

--- tests/test_board_routes.py
+++ tests/test_board_routes.py
@@ -10,3 +10,20 @@ def test_get_all_boards_no_saved_boards(client):
     # Assert
     assert response.status_code == 200
     assert response_body == []
+
+def test_get_all_boards_one_saved_board(client, one_board):
+    # Act
+    response = client.get("/boards")
+    response_body = response.get_json()
+
+    # Assert
+    assert response.status_code == 200
+    assert len(response_body) == 1
+    assert response_body == [
+        {
+            "id": 1,
+            "title": "Learn how to cook",
+            "owner": "Michael",
+            "cards": []
+        }
+    ]
