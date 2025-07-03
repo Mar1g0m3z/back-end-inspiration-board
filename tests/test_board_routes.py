fa70d7fe1382fafda571aca8f9c24077ccfd1eb1
sashavershkova <a.m.vershkova@gmail.com>
2025-07-02 19:47:00 -0700

    add more assertions to test_get_all_boards_three_saved_boards

--- tests/test_board_routes.py
+++ tests/test_board_routes.py
@@ -27,3 +27,26 @@ def test_get_all_boards_one_saved_board(client, one_board):
             "cards": []
         }
     ]
+
+def test_get_all_boards_three_saved_boards(client, three_boards):
+    # Act
+    response = client.get("/boards")
+    response_body = response.get_json()
+
+    # Assert
+    assert response.status_code == 200
+    assert len(response_body) == 3
+
+    expected_boards = [
+        {"title": "Find new hobbies", "owner": "Anna"},
+        {"title": "Become more social", "owner": "Fedor"},
+        {"title": "Find a dream job", "owner": "Sasha"}
+    ]
+
+    for board in expected_boards:
+        assert any(
+            returned["title"] == board["title"]
+            and returned["owner"] == board["owner"]
+            and returned["cards"] == []
+            for returned in response_body
+        )
